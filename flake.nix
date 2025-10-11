d5c7dc3f1ecc28e76db975bd375a2f818ffe336a
anttiharju <antti@harju.io>
2025-10-11 19:25:39 +0300

    Apply nixfmt

--- flake.nix
+++ flake.nix
@@ -3,25 +3,43 @@
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs, ... }:
+  outputs =
+    { self, nixpkgs, ... }:
     let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-    in {
-      devShells = forAllSystems (system: let
-        pkgs = import nixpkgs { inherit system; };
-      in {
-        default = pkgs.mkShell {
-          packages = with pkgs; [ rustc cargo rustfmt rust-analyzer clippy lefthook ];
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              rustc
+              cargo
+              rustfmt
+              rust-analyzer
+              clippy
+              lefthook
+            ];
 
-          env = {
-            RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+            env = {
+              RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+            };
+
+            shellHook = ''
+              lefthook install
+            '';
           };
-
-          shellHook = ''
-            lefthook install
-          '';
-        };
-      });
+        }
+      );
     };
 }
